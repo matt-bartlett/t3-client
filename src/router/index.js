094743c0c3f6b05462df39806c4364b609c85dc0
Matt Bartlett <info@mattbartlett.net>
2017-10-15 09:51:59 +0100

    Vue Router set to history mode

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Search from '@/components/Search'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -27,5 +28,8 @@ export default new Router({
       path: '*',
       redirect: '/'
     }
-  ]
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
 })
