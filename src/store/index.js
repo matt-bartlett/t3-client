9957f4fd47d30ad11d05557a7b13ac88fc8cf81b
Matt Bartlett <info@mattbartlett.net>
2018-02-15 21:33:58 +0000

    Removed reference to banner in Vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,10 @@ Vue.use(Vuex)
 
 import playlists from './modules/playlists'
 import tracks from './modules/tracks'
-import banner from './modules/banner'
 
 export default new Vuex.Store({
   modules: {
     playlists: playlists,
-    tracks: tracks,
-    banner: banner
+    tracks: tracks
   }
 })
